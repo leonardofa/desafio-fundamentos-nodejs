4b392f3f80dacaebde19fd1123b0c34cd6bcc613
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-05-09 19:24:36 -0300

    Entrega do desafio

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,25 +1,35 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
+const transactionsRepository = new TransactionsRepository();
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(err.status || 400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+    return response.json(
+      createTransactionService.execute({ title, value, type }),
+    );
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(err.status || 400).json({ error: err.message });
   }
 });
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,34 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!title) {
+      throw Error('Title is required');
+    }
+
+    if (!value) {
+      throw Error('Value is required');
+    }
+
+    if (typeof value !== 'number' || value <= 0) {
+      throw Error('Value is not valid');
+    }
+
+    if (!type) {
+      throw Error('Type is required');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type is not valid');
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('There is no value to outcome transaction');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
